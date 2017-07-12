8b82b9def59239614cb5af2e140a3aa712cea6dd
Sina <Sinadarvi@gmail.com>
2017-07-12 16:54:20 +0300

    basic

--- src/Main.kt
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Sina on 7/11/17.
- */
-
-fun main(args: Array<String>){
-
-    println("hello World")
-}
---
+++ src/com/kotlinfarsi/introduction/basic/variable.kt
@@ -0,0 +1,9 @@
+package com.kotlinfarsi.introduction.basic
+
+/**
+ * Created by Sina on 7/12/17.
+ */
+
+fun main(args: Array<String>) {
+    println("hiii")
+}
